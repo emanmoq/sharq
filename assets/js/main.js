bfc5772ed42f1cc9773f4cc43949f1aa9be5fccd
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-03-22 04:09:08 -0700

    index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 jQuery(function ($) {
+  $(".heroSlider").owlCarousel({
+    nav: false,
+    loop: true,
+    dots: false,
+    items: 1
 
-
-
+  });
+  $(".coursesSlider").owlCarousel({
+    nav: true,
+    loop: true,
+    dots: false,
+    items: 3,
+    margin:25,
+    respnsive:{
+      0:{
+        items:1
+      },
+      6:{
+        items:2
+      }
+    }
+  });
 });
+
 
