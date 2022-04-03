2fa872d111f1db0cb7130e22f5556810b2b5cde2
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-04-03 00:56:25 -0700

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,27 @@ jQuery(function ($) {
     },
 
   });
+  $(".intersetcoursesSlider").owlCarousel({
+    nav: false,
+    loop: false,
+    dots: false,
+    autoplay:true,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    items: 4,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1
+      },
+      700: {
+        items: 2
+      },
+      1000: {
+        items: 3,
+      }
+    },
+
+  });
 
   $(".certificateSlider").owlCarousel({
     nav: true,
@@ -87,6 +108,26 @@ jQuery(function ($) {
     },
 
   });
+  $(".instructorSlider").owlCarousel({
+    nav: false,
+    loop: false,
+    dots: false,
+    items: 3,
+    margin: 25,
+    responsive: {
+      0: {
+        items: 1
+      },
+      700: {
+        items: 2
+      },
+      1000: {
+        items: 3,
+      },
+
+    },
+
+  });
   $(".nav-slider").owlCarousel({
     nav: false,
     loop: false,
@@ -95,17 +136,55 @@ jQuery(function ($) {
     items: 2
 
   });
+  
   $(window).scroll(function () {
     scrollTop = $(window).scrollTop();
     if (scrollTop > 1000) {
       $(".up").css("opacity", "1")
     }
-    else {
+    else if (scrollTop < 1000)  {
       $(".up").css("opacity", "0")
 
     }
-    $(".up").on("click", function () { $("html, body").animate({ scrollTop: 0 }, "slow"); return false; });
+    if (scrollTop > $("header").height()-10){
+      $(".courseDetailsBody .innerHeader").addClass("removeHeader");
+      $(".courseDetailsBody .scrolableHeader").css("display","flex")
+      $(".courseDetailsBody .mainHeader").addClass("removeHeader");
+      $(".courseDetailsBody .coursesTabsSection").addClass("scrollNav");
+      $(".videoSec").addClass("scrollVideo");
+      
+    }
+    else{
+      $(".courseDetailsBody  .innerHeader").removeClass("removeHeader");
+      $(".courseDetailsBody .mainHeader").removeClass("removeHeader");
+      $(".courseDetailsBody .coursesTabsSection").removeClass("scrollNav");
+      $(".courseDetailsBody .scrolableHeader").css("display","none");
+      $(".videoSec").removeClass("scrollVideo");
 
+    }
+
+    function onScroll(event){
+      var scrollPosition = $(document).scrollTop();
+    $('.courseDetailsBody .coursepage-tabs .nav-link').each(function () {
+      console.log($(this).attr('href')); 
+      var refElement = $(this).attr("href");
+    //   console.log($(this).attr("href")); //log
+      if (refElement.length() && refElement.position().top-100 <= scrollPosition) {
+      $('.courseDetailsBody .coursepage-tabs .nav-link').removeClass("active");
+      $(this).addClass("active");
+      }
+      else if( $(document).scrollTop() <= 100){
+        $('.courseDetailsBody .coursepage-tabs .nav-link').removeClass("active");
+      }
+      else{
+      $(this).removeClass("active"); 
+    
+      }
+    });
+  }
+  $(document).on("scroll", onScroll);
+        // alert(scrollTop);
+    $(".up").on("click", function () { $("html, body").animate({ scrollTop: 0 }, "slow"); return false; }); 
   });
 
   $(".langBtn").click(function () {
@@ -157,11 +236,88 @@ jQuery(function ($) {
       $(this).not(this).find('span:first').removeClass('lni-chevron-up').addClass('lni-chevron-down');
       $(this).find('span:first').toggleClass('lni lni-chevron-up lni lni-chevron-down');
   })
-
+  $(".CurriculumLink").click(function(){
+    $(this).not(this).find('span:first').removeClass('lni-chevron-down').addClass('lni-chevron-up');
+    $(this).find('span:first').toggleClass('lni lni-chevron-down lni lni-chevron-up');
+  })
   $('.faqHeader').click(function () {
-      $(this).parent().find('.faqbody').slideToggle(400);
+    $(this).parent().find('.faqbody').slideToggle(400);
+});
+$(".faqHeader").on("click", "a", function(e) { e.preventDefault() });
+
+  $('.CurriculumLink').click(function () {
+      $(this).parent().find('.innerCurriculumList').slideToggle(400);
   });
-  $(".faqHeader").on("click", "a", function(e) { e.preventDefault() });
+  $(".CurriculumLink").on("click", "a", function(e) { e.preventDefault() });
+  $(".radioBtnContainer").on("click", "a", function(e) { e.preventDefault() });
+  
+  $(".readOnlyRating").starRating({
+    starSize: 16,
+    activeColor: '#FFD93F',
+    hoverColor: '#FFD93F',
+    ratedColor: '#FFD93F',
+    emptyColor: 'transparent',
+    starShape: 'straight',
+    strokeColor:'#FFD93F',
+    strokeWidth:9,
+    ratedColors: '#FFD93F',
+    useGradient: false,
+    readOnly: true,
+    callback: function (currentRating, $el) {
+      // make a server call here
+    }
+  });
+  $(".lectureReviewsReadOnly").starRating({
+    starSize: 16,
+    activeColor: '#FFD93F',
+    hoverColor: '#FFD93F',
+    ratedColor: '#FFD93F',
+    emptyColor: 'transparent',
+    starShape: 'straight',
+    strokeColor:'#FFD93F',
+    strokeWidth:9,
+    ratedColors: '#FFD93F',
+    useGradient: false,
+    readOnly: true,
+    callback: function (currentRating, $el) {
+      // make a server call here
+    }
+  });
+  $(".learnerReviewsReadOnly").starRating({
+    starSize: 16,
+    rtl: true,
+    activeColor: '#FFB13E',
+    hoverColor: '#FFB13E',
+    ratedColor: '#FFB13E',
+    emptyColor: 'transparent',
+    starShape: 'straight',
+    strokeColor:'#FFB13E',
+    strokeWidth:9,
+    ratedColors: '#FFD93F',
+    useGradient: false,
+    readOnly: true,
+    callback: function (currentRating, $el) {
+      // make a server call here
+    }
+  });
+  $(".leaveReviewRating").starRating({
+    starSize: 16,
+    activeColor: '#FFB13E',
+    hoverColor: '#FFB13E',
+    ratedColor: '#FFB13E',
+    emptyColor: 'transparent',
+    starShape: 'straight',
+    strokeColor:'#FFB13E',
+    strokeWidth:9,
+    ratedColors: '#FFD93F',
+    useGradient: false,
+    disableAfterRate:true,
+    useFullStars: true,
+
+    callback: function (currentRating, $el) {
+      // make a server call here
+    }
+  });
 });
 
 
